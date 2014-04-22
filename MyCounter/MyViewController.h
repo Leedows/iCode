4ebcdee1be95cb28d44f595309c7acc88ba3c1da
Leedows <shuan5189@163.com>
2014-04-22 23:50:38 +0800

    Add MyViewController.h Add MyViewController.m

---
+++ MyCounter/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  MyCounter
+//
+//  Created by lee on 14-4-22.
+//  Copyright (c) 2014年 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
